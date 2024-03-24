752117804a543ece5ac0c7c06576f1af93eb9d76
Alexandra Makey <aliaksandra.makey@gmail.com>
2024-03-24 23:49:24 +0400

    Created base project and removed storyboard references

---
+++ FoodDelivery/FoodDelivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Александра Макей on 23.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
